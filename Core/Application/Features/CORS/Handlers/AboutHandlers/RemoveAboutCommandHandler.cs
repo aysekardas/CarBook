2600c3a7a45eceb7344ad3c041fd9cf95d7492a1
aysekardas <aysekardas7@gmail.com>
2024-01-31 22:45:26 +0300

    add crud operations to about command

---
+++ Core/Application/Features/CORS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.CORS.Commands.AboutComands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task Handle(RemoveAboutCommand command)
+        {
+           var value = await _repository.GetByIdAsync(command.Id);
+            await _repository.RemoveAsync(value);
+
+        }
+    }
+}
---
+++ Core/Application/Features/CORS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.CORS.Commands.AboutComands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.AboutID);
+            values.Description = command.Description;
+            values.ImageUrl = command.ImageUrl;
+            values.Title = command.Title;
+            await _repository.UpdateAsync(values); 
+        }
+    }
+}

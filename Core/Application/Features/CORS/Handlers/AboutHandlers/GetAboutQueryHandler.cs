554da49e71ae88f06b8e4b547b734e0bab19c4e0
aysekardas <aysekardas7@gmail.com>
2024-01-31 22:16:06 +0300

    add features/cors

---
+++ Core/Application/Features/CORS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.CORS.Results.AboutResult;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        //Handle CORS'de CRUD işlemlerini gerçekleştirirken kullanılan method ismi
+        public async Task< List<GetAboutByIdQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAboutByIdQueryResult
+                {
+                AboutID = x.AboutID,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl,
+                Title = x.Title
+            }).ToList();
+        }
+    }
+}
---
+++ Core/Application/Features/CORS/Queries/AboutQueries/GetAboutByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Queries.AboutQueries
+{
+    public class GetAboutByIdQuery
+    {
+        public GetAboutByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+        //bir nesne örneği üzerinden Id çağıracağım için ctor oluşturdum.
+
+
+    }
+}

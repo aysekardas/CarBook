a28ad356202d0b9f4937030ab905e9ed209e47d3
aysekardas <aysekardas7@gmail.com>
2024-01-31 22:25:14 +0300

    add getaboutbyidqueryhandler

---
+++ Core/Application/Features/CORS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.CORS.Queries.AboutQueries;
+using Application.Features.CORS.Results.AboutResult;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.AboutHandlers
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetAboutByIdQueryResult
+            {
+                AboutID = values.AboutID,
+                Description = values.Description,
+                ImageUrl = values.ImageUrl,
+                Title = values.Title
+            };
+
+        }
+    }
+}

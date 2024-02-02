519080eabe0939a1e8a7bb6b21ebf24ea6de6aa1
aysekardas <aysekardas7@gmail.com>
2024-02-02 21:10:31 +0300

    Adding constructor methods to program.cs

--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -1,6 +1,16 @@
+using Application.Features.CORS.Handlers.AboutHandlers;
+using Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

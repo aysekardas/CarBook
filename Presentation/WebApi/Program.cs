43b2575af32c48474c175edb8e8f75cd287f06b9
aysekardas <aysekardas7@gmail.com>
2024-02-03 22:52:51 +0300

    update program.cs

--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -1,4 +1,6 @@
 using Application.Features.CORS.Handlers.AboutHandlers;
+using Application.Features.CORS.Handlers.BannerHandlers;
+using Application.Features.CORS.Queries.BannerQueries;
 using Application.Interfaces;
 using Persistence.Context;
 using Persistence.Repository;
@@ -15,6 +17,13 @@ builder.Services.AddScoped<GetAboutByIdQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+
+builder.Services.AddScoped<GetBannerByIdQuery>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

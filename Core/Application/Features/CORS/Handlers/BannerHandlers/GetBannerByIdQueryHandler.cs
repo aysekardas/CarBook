8a3f1c5b5fa7d975d0bb8d48543b15f835457e40
aysekardas <aysekardas7@gmail.com>
2024-02-03 22:10:30 +0300

    add cors for banner

---
+++ Core/Application/Features/CORS/Handlers/BannerHandlers/GetBannerByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.CORS.Queries.BannerQueries;
+using Application.Features.CORS.Results.BannerResults;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.BannerHandlers
+{
+    public class GetBannerByIdQueryHandler
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public GetBannerByIdQueryHandler(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetBannerByIdQueryResult> Handle(GetBannerByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetBannerByIdQueryResult
+            {
+                BannerID = values.BannerID,
+                Description = values.Description,
+                Title = values.Title,
+                VideoDescription = values.VideoDescription,
+                VideoUrl = values.VideoUrl
+            };
+        }
+    }
+}
---
+++ Core/Application/Features/CORS/Handlers/BannerHandlers/UpdateBannerCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.CORS.Commands.BannerCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.BannerHandlers
+{
+    public class UpdateBannerCommandHandler
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public UpdateBannerCommandHandler(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateBannerCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.BannerID);
+            values.Description = command.Description;
+            values.Title = command.Title;
+            values.VideoUrl = command.VideoUrl;
+            values.VideoDescription = command.VideoDescription;
+            await _repository.UpdateAsync(values);  
+        }
+    }
+}

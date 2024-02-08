19a37be2f2163d8f1219a16bb63dee69402a54ff
aysekardas <aysekardas7@gmail.com>
2024-02-08 22:28:47 +0300

    add contact handlers

---
+++ Core/Application/Features/CORS/Handlers/ContactHandlers/GetContactQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.CORS.Results.ContactResult;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.ContactHandlers
+{
+    public class GetContactQueryHandler
+    {
+        private readonly IRepository<Contact> _repository;
+
+        public GetContactQueryHandler(IRepository<Contact> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetContactQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetContactQueryResult
+            {
+                ContactID = x.ContactID,
+                Name = x.Name,
+                Message = x.Message,
+                SendDate = x.SendDate,
+                Subject = x.Subject,
+                Email = x.Email 
+
+
+            }).ToList();
+        }
+    }
+}

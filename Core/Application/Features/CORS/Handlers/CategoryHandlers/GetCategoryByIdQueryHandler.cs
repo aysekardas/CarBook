92b0b209a44b387fb795463c6d52bde7e68d6a85
aysekardas <aysekardas7@gmail.com>
2024-02-08 21:47:27 +0300

    add category handlers

---
+++ Core/Application/Features/CORS/Handlers/CategoryHandlers/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.CORS.Queries.CategoryQueries;
+using Application.Features.CORS.Results.CategoryResult;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.CategoryHandlers
+{
+    public class GetCategoryByIdQueryHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public GetCategoryByIdQueryHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetCategoryByIdQueryResult> Handle(GetCategoryByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetCategoryByIdQueryResult
+            {
+                CategoryID = values.CategoryID,
+                Name = values.Name
+
+            };
+        }
+    }
+}
---
+++ Core/Application/Features/CORS/Handlers/CategoryHandlers/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.CORS.Commands.CategoryCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CORS.Handlers.CategoryHandlers
+{
+    public class UpdateCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public UpdateCategoryCommandHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateCategoryCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.CategoryID);
+            values.Name = command.Name;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}

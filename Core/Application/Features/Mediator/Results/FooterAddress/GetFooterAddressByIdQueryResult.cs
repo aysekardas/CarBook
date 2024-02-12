26c6b09a063c866764a5e495b0276182ad4ceb96
aysekardas <aysekardas7@gmail.com>
2024-02-12 22:49:06 +0300

    add footer address results

---
+++ Core/Application/Features/Mediator/Results/FooterAddress/GetFooterAddressByIdQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Results.FooterAddress
+{
+    public class GetFooterAddressByIdQueryResult
+    {
+        public int FooterAdressID { get; set; }
+        public string Description { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}

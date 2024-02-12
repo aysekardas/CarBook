1461415783acdfaaa3c011b6916ec56ec621c465
aysekardas <aysekardas7@gmail.com>
2024-02-12 23:32:44 +0300

    add footer address handlers

---
+++ Core/Application/Features/Mediator/Handlers/FooterAddressHandlers/GetFooterAddressQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.Mediator.Queries.FooterAddressQueries;
+using Application.Features.Mediator.Results.FooterAddress;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.FooterAddressHandlers
+{
+    public class GetFooterAddressQueryHandler : IRequestHandler<GetFooterAddressQuery, List<GetFooterAddressQueryResult>>
+    {
+        private readonly IRepository<FooterAdress> _repository;
+
+        public GetFooterAddressQueryHandler(IRepository<FooterAdress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetFooterAddressQueryResult>> Handle(GetFooterAddressQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetFooterAddressQueryResult
+            {
+                Adress = x.Adress,
+                Description = x.Description,
+                Email = x.Email,
+                FooterAdressID = x.FooterAdressID,
+                Phone = x.Phone
+            }).ToList();
+        }
+    }
+}
---
+++ Core/Application/Features/Mediator/Handlers/FooterAddressHandlers/UpdateFooterAddressCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Mediator.Commands.FooterAddressCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.FooterAddressHandlers
+{
+    public class UpdateFooterAddressCommandHandler : IRequestHandler<UpdateFooterAddressCommand>
+    {
+        private readonly IRepository<FooterAdress> _repository;
+
+        public UpdateFooterAddressCommandHandler(IRepository<FooterAdress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateFooterAddressCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.FooterAdressID);
+            values.Phone = request.Phone;
+            values.Adress = request.Adress;
+            values.Description = request.Description;
+            values.Email = request.Email;
+            await _repository.UpdateAsync(values);
+        }
+
+    }
+}

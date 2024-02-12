2f284a1ef4b3b0b08bf7edb5bb5b1ad679dec897
aysekardas <aysekardas7@gmail.com>
2024-02-12 23:55:06 +0300

    add footer addresses controller

---
+++ Presentation/WebApi/Controllers/FooterAdressesController.cs
@@ -0,0 +1,62 @@
+﻿using Application.Features.Mediator.Commands.FooterAddressCommands;
+using Application.Features.Mediator.Queries.FooterAddressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAdressesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FooterAdressesController(IMediator mediaator)
+        {
+            _mediator = mediaator;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressList()
+        {
+            var values =await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Alt adres bilgisi eklendi");
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddress(int id)
+        {
+            var value = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+             await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok("Alt adres bilgisi silindi");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateFooterAdress(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Alt adres bilgisi güncellendi");
+        }
+             
+             
+
+    }
+}

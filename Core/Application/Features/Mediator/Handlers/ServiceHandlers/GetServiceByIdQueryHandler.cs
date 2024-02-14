e9ddd3885b6876c4a74de7cc31237987f1490228
aysekardas <aysekardas7@gmail.com>
2024-02-15 00:39:53 +0300

    add service handlers

---
+++ Core/Application/Features/Mediator/Handlers/ServiceHandlers/GetServiceByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Mediator.Queries.ServiceQueries;
+using Application.Features.Mediator.Results.ServiceResults;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.ServiceHandlers
+{
+    public class GetServiceByIdQueryHandler : IRequestHandler<GetServiceByIdQuery, GetServiceByIdQueryResult>
+    {
+        private readonly IRepository<Service> _repository;
+
+        public GetServiceByIdQueryHandler(IRepository<Service> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetServiceByIdQueryResult> Handle(GetServiceByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetServiceByIdQueryResult
+            {
+                Description = values.Description,
+                IconUrl = values.IconUrl,
+                ServiceID = values.ServiceID,
+                Title = values.Title
+            };
+        }
+    }
+}

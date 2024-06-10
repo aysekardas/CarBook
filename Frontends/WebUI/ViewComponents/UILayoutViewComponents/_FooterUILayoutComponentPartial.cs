750cd6e4f36928c7166b3f0de276aa4f251fa78e
aysekardas <aysekardas7@gmail.com>
2024-06-10 23:10:18 +0300

    add footer component  &  script component

---
+++ Frontends/WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            {
+                return View();
+            }
+        }
+    }
+}

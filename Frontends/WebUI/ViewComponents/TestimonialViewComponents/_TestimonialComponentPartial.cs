1e69a6591acd9b8031b7387226261b12f287eb91
aysekardas <aysekardas7@gmail.com>
2024-06-25 23:06:10 +0300

    update become a driver

---
+++ Frontends/WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+           _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7279/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+
+
+
+        }
+    }
+}

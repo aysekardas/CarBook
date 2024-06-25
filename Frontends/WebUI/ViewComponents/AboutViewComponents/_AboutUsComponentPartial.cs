7cb12a2b62de0b6be601eed7a63680b45c647c36
aysekardas <aysekardas7@gmail.com>
2024-06-25 21:34:32 +0300

    add about consume

--- Frontends/WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7279/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                //DeserializeObject : Api türünde gelen veriyi text türünde viewde göstermek istediğimiz için
+                //SerializeObject : Inputlara girdiğimiz veriyi apiye göndermek istersek bir ekleme veya güncelleme işlemi yapmak istersek 
+            }
             return View();
         }
     }

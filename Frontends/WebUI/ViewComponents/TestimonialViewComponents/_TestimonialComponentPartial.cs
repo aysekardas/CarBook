775d25e7aa0c0f6feddab2e3bfd9333bb3f5594b
aysekardas <aysekardas7@gmail.com>
2024-06-26 21:30:36 +0300

    add testimonial lists

--- Frontends/WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.TestimonialViewComponents
@@ -12,14 +13,14 @@ namespace WebUI.ViewComponents.TestimonialViewComponents
            _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7279/api/Testimonials");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
                 return View(values);
 
             }

4563e4155b910081508f759ac989fc8cecea38be
aysekardas <aysekardas7@gmail.com>
2024-06-30 00:14:43 +0300

    homepage and banner area

--- Frontends/WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultCoverUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7279/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+
+            }
+
             return View();
         }
     }

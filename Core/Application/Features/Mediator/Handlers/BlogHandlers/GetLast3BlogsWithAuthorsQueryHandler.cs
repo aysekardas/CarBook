0fa370b5c1b045d0be278a9a01e7336eb625cad2
aysekardas <aysekardas7@gmail.com>
2024-07-29 22:17:56 +0300

    Fetch last 3 blogs including author details

---
+++ Core/Application/Features/Mediator/Handlers/BlogHandlers/GetLast3BlogsWithAuthorsQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.Mediator.Queries.BlogQueries;
+using Application.Features.Mediator.Results.BlogResults;
+using Application.Interfaces.BlogInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetLast3BlogsWithAuthorsQueryHandler : IRequestHandler<GetLast3BlogsWithAuthorsQuery, List<GetLast3BlogsWithAuthorsQueryResult>>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetLast3BlogsWithAuthorsQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLast3BlogsWithAuthorsQueryResult>> Handle(GetLast3BlogsWithAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetLast3BlogsWithAuthors();
+            return values.Select(x => new GetLast3BlogsWithAuthorsQueryResult
+            {
+                BlogID = x.BlogID,
+                AuthorID = x.AuthorID,
+                CategoryID = x.CategoryID,
+                CoverImageUrl = x.CoverImageUrl,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                AuthorName = x.Author.Name
+
+            }).ToList();
+        }
+    }
+}

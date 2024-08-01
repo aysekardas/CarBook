0c5fa88d7ab22f7a192f73b42e57e1c7c9960179
aysekardas <aysekardas7@gmail.com>
2024-08-01 23:40:09 +0300

    consume API to fetch cars with prices

--- Frontends/WebUI/Controllers/CarController.cs
+++ Frontends/WebUI/Controllers/CarController.cs
@@ -1,4 +1,5 @@
 ﻿using Dto.CarDtos;
+using Dto.CarPricingDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -18,11 +19,11 @@ namespace WebUI.Controllers
             ViewBag.v1 = "Araçlarımız";
             ViewBag.v2 = "Aracınızı Seçiniz";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7279/api/Cars/GetCarWithBrand");
+            var responseMessage = await client.GetAsync("https://localhost:7279/api/CarPricings");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
                 return View(values);
 
             }
--- Infrastructure/Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ Infrastructure/Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -21,7 +21,7 @@ namespace Persistence.Repositories.CarPricingRepositories
 
 		public List<CarPricing> GetCarPricingWithCars()
 		{
-			var values = _context.CarPricings.Include(x=>x.Car).ThenInclude(y=>y.Brand).Include(x=>x.Pricing).ToList();
+			var values = _context.CarPricings.Include(x=>x.Car).ThenInclude(y=>y.Brand).Include(x=>x.Pricing).Where(z=>z.PricingID == 2).ToList();
 			return values;
 			
 		}

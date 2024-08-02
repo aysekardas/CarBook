0d96612665521fc1d21d172b4a75b89ddd726546
aysekardas <aysekardas7@gmail.com>
2024-08-02 16:32:12 +0300

    list blogs on the frontend

---
+++ Frontends/WebUI/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.Controllers
+{
+	public class BlogController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public BlogController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			ViewBag.v1 = "Bloglar";
+			ViewBag.v2 = "Blogları Görüntüleyin";
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7279/api/Blogs/GetAllBlogsWithAuthorList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+				return View(values);
+            }
+            return View();
+		}
+	}
+}

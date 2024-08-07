3ccf5ac118b4d1bf265fd31347a0b9039c23339a
aysekardas <aysekardas7@gmail.com>
2024-08-07 23:24:17 +0300

    add initial tag cloud list setup

--- Frontends/WebUI/ViewComponents/BlogViewComponents/_BlogDetailCloudTagByBlogComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/BlogViewComponents/_BlogDetailCloudTagByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailCloudTagByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailCloudTagByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7279/api/TagClouds/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
--- Frontends/WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsTagCloudComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsTagCloudComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7279/api/TagClouds/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
